221a8a5ad589ef9a6d9d9a95c93c4bd6dfaacc93
MichMery <mariasosin@wp.pl>
2019-03-18 22:31:02 +0100

    Potegowanie.java

--- src/CiagFibonacciego.java
+++ src/CiagFibonacciego.java
@@ -1,5 +1,10 @@
 public class CiagFibonacciego {
 	public static int fibonaccie(int index) {
-		return 0;
-	}
+		if(index==0)
+			return 0;
+		if ((index==1)||(index==2))
+			return 1;
+			else
+			return fibonaccie(index-2)+fibonaccie(index-1);
+			}
 }
--- src/Potegowanie.java
+++ src/Potegowanie.java
@@ -1,6 +1,25 @@
 
 public class Potegowanie {
 	public static int potega(int podstawa, int potega) {
-		return 1;
-	}
+		
+		while (potega>=0){
+			potega=podstawa*=potega;
+			if(potega==2) {
+				potega=podstawa*podstawa;
+						}
+			if (potega==1) {
+				potega=podstawa;
+			}
+				if(potega==0) {
+					potega=1;
+				}
+				
+			}
+				
+			while (potega<0) {
+				potega=1/(podstawa*=potega);
+			}	
+			
+			return potega;
+			}
 }
